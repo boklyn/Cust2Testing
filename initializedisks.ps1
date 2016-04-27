c844dc0f92e049f093407e29af751ab145490832
boklyn <boklynw@microsoft.com>
2016-04-27 16:14:03 -0400

    hhk

--- initializedisks.ps1
+++ initializedisks.ps1
@@ -1,17 +1,19 @@
 ﻿param(
-    [parameter(Mandatory=$true)]
-    [int[]]$disks
+    [parameter(Mandatory=$false)]
+    [string]$disks
 )
 
-$friendlypoolname = "primarydiskpool"
+$friendlypoolname = "priemarydiskpool"
 $dktopool = Get-PhysicalDisk | Where-Object {$_.CanPool -eq $true}
 $subsystem = Get-StorageSubSystem
 New-StoragePool -FriendlyName $friendlypoolname -StorageSubSystemFriendlyName $subsystem.FriendlyName -PhysicalDisks $dktopool
 $protype = "fixed"
 $resilsetting = "Simple"
 $count = 0;
-foreach ($disk in $disks){    
-    #$disk = [int]$disk
+Start-Sleep -s 10
+$adisks = $disks.Split(",")
+foreach ($disk in $adisks){
+    $disk = [int32]$disk 
     $vdisk = New-VirtualDisk -StoragePoolFriendlyName $friendlypoolname -FriendlyName ("DataDisk_"+$count)  -Size ($disk*1GB) -ResiliencySettingName $resilsetting -ProvisioningType $protype
     Initialize-Disk -VirtualDisk (Get-VirtualDisk -FriendlyName ("DataDisk_"+$count))
     $curdisk = get-disk -VirtualDisk $vdisk
