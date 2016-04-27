f73a439aef4785cef40fb3049931b433b1a7b414
boklyn <boklynw@microsoft.com>
2016-04-27 14:14:54 -0400

    bugfixxes

--- initializedisks.ps1
+++ initializedisks.ps1
@@ -6,11 +6,9 @@
 $friendlypoolname = "primarydiskpool"
 $dktopool = Get-PhysicalDisk | Where-Object {$_.CanPool -eq $true}
 $subsystem = Get-StorageSubSystem
-#New-StoragePool -FriendlyName $friendlypoolname -StorageSubSystemFriendlyName $subsystem.FriendlyName -PhysicalDisks $dktopool
+New-StoragePool -FriendlyName $friendlypoolname -StorageSubSystemFriendlyName $subsystem.FriendlyName -PhysicalDisks $dktopool
 $protype = "fixed"
 $resilsetting = "Simple"
-$disks | Out-File c:\scripts\test.txt
-#$disks = @(2000, 500, 5000, 1000, 300, 200)
 $count = 0;
 foreach ($disk in $disks){    
     #$disk = [int]$disk
